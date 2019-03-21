69b45e5e9d442ce885e9cfcb075ecde3f47d7d69
PopAps <23138@tkschool.ac.th>
2019-03-22 01:16:23 +0700

    folder js and router

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser')
 
 
 var login = require('./router/login')
+var buy = require('./router/buy')
+var sale = require('./router/sale')
+var repair = require('./router/repair')
+var regis = require('./router/regis_car')
 var path = require('path');
 app.use(express.static('public'));
 app.get('/', function (req, res) {
@@ -12,6 +16,10 @@ app.get('/', function (req, res) {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(login)
+app.use(buy)
+app.use(sale)
+app.use(repair)
+app.use(regis)
 app.listen(process.env.PORT || 3000, () => {
   console.log('Start server at port 3000.')
 })
