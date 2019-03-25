5fd4bb2dcd6e7e451769282d65d609cdd7f29897
PopAps <23138@tkschool.ac.th>
2019-03-26 00:56:15 +0700

    bill invoice V3

--- public/js/billAndInvoice.js
+++ public/js/billAndInvoice.js
@@ -8,31 +8,31 @@ function getTableBill() {
     var parsed = JSON.parse(json);
 
     var count = Object.keys(parsed.bill).length;
-   
-  /**************** css*////************** */
+
+    /**************** css*////************** */
     var td = "BVtd"
-    var th ="BVth"
+    var th = "BVth"
     var table = "BVtable"
     var tr = "BVtr"
-/**************** *////************** */
-    var tableHeader = "<table class="+table+"><tr><th class="+th+">รหัสรถยนต์</th ><th class="+th+">ชื่อรถยนต์/รุ่น</th><th class="+th+">เลขทะเบียน</th><th class="+th+">เลขเครื่อง</th><th class="+th+">น้ำหนัก</th></tr>";
+    /**************** *////************** */
+    var tableHeader = "<table class=" + table + "><tr><th class=" + th + ">รหัสรถยนต์</th ><th class=" + th + ">ชื่อรถยนต์/รุ่น</th><th class=" + th + ">เลขทะเบียน</th><th class=" + th + ">เลขเครื่อง</th><th class=" + th + ">น้ำหนัก</th></tr>";
     var tableContent = "";
 
-    
-  
+
+
     for (i = 0; i < count; i++) {
-        tableContent = tableContent + "<tr class="+tr+">" 
-        +"<td class="+td+">" + parsed.bill[i].ID_TRN_buy_bill + 
-        "</td> <td class="+td+">" + parsed.bill[i].model + 
-        "</td> <td class="+td+">"+ parsed.bill[i].license_plate + 
-        "</td> <td class="+td+">"+ parsed.bill[i].ID_MST_stock +
-        "</td> <td class="+td+">  "+ parsed.bill[i].weight +  
-        "</tr>";
+        tableContent = tableContent + "<tr class=" + tr + ">"
+            + "<td class=" + td + ">" + parsed.bill[i].ID_TRN_buy_bill +
+            "</td> <td class=" + td + ">" + parsed.bill[i].model +
+            "</td> <td class=" + td + ">" + parsed.bill[i].license_plate +
+            "</td> <td class=" + td + ">" + parsed.bill[i].ID_MST_stock +
+            "</td> <td class=" + td + ">  " + parsed.bill[i].weight +
+            "</tr>";
     }
 
     var tableFooter = "</table>";
     document.getElementById("BVtable").innerHTML = tableHeader + tableContent + tableFooter;
-    
+
     ////query
     document.getElementById("priceHeader").innerHTML = "30000"
     document.getElementById("invB").innerHTML = " -"
@@ -45,62 +45,79 @@ function getTableBill() {
 
 function getTableInvoice() {
 
-    var query = location.search.substring(1);
-    var id = query.split("%20")
-    
-    var query = {
-        id:id
-    }
-    const http = new XMLHttpRequest()
-    http.open('GET', 'http://localhost:3000/api/buy/invoice/getItem')
-    http.setRequestHeader('Content-type', 'application/json')
-    http.send(JSON.stringify(query)) // Make sure to stringify
-    http.onload = function() {
-        // Do whatever with response
-        alert(http.responseText)
+    // var query = location.search.substring(1);
+    // var ID_TRN_buy_bill = query.split("%20")
+
+    var id = {
+        id: "BYB00010"
     }
 
-    var json = '{ "bill" : [' +
-        '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000" },' +
-        '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000"  },' +
-        '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000"  } ]}';
 
-    var parsed = JSON.parse(json);
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "http://localhost:3000/api/buy/invoice/getItem",
+        data: JSON.stringify(id),
+        dataType: 'json',
+        success: function (res) {
+            var bill;
+            var stock;
+            var customer;
+            console.log(res.length)
 
 
-    var count = Object.keys(parsed.bill).length;
-    var td = "BVtd"
-    var th ="BVth"
-    var table = "BVtable"
-    var tr = "BVtr"
+            bill = res[0];
+            customer = res[2];
+            stock = res[4];
 
-    var tableHeader = "<table class="+table+"><tr><th class="+th+">ลำดับที่</th ><th class="+th+">เลขทะเบียน</th><th class="+th+">ประเภทรถ</th><th class="+th+">สี</th><th class="+th+">ราคา</th></tr>";
-    var tableContent = "";
+            console.log(bill[0].VAT);
+            var json = '{ "bill" : [' +
+                '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000" },' +
+                '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000"  },' +
+                '{ "ID_TRN_taxlnvoice": 1231 , "license_plate":"กกต 6001" ,"type":"A","color":"ดำ","price":"10000"  } ]}';
 
-    var pr  = 0
+            var parsed = JSON.parse(json);
 
-    for (i = 0; i < count; i++) {
-        tableContent = tableContent + "<tr class="+tr+">" 
-        +"<td class="+td+">" + parsed.bill[i].ID_TRN_taxlnvoice + 
-        "</td> <td class="+td+">" + parsed.bill[i].license_plate + 
-        "</td> <td class="+td+">"+ parsed.bill[i].type + 
-        "</td> <td class="+td+">"+ parsed.bill[i].color +
-        "</td> <td class="+td+">  "+ parsed.bill[i].price +  
-        "</tr>";
-        pr = pr + parseInt(parsed.bill[i].price);
-    }
 
-    var tableFooter = "</table>";
+            var count = Object.keys(parsed.bill).length;
+            var td = "BVtd"
+            var th = "BVth"
+            var table = "BVtable"
+            var tr = "BVtr"
 
-    document.getElementById("BVtable").innerHTML = tableHeader + tableContent + tableFooter;
-    document.getElementById("priceAll").innerHTML = pr;
-    var inv = pr*(5/100)
-    document.getElementById("inv").innerHTML =  inv;
+            var tableHeader = "<table class=" + table + "><tr><th class=" + th + ">ลำดับที่</th ><th class=" + th + ">เลขทะเบียน</th><th class=" + th + ">ประเภทรถ</th><th class=" + th + ">สี</th><th class=" + th + ">ราคา</th></tr>";
+            var tableContent = "";
 
-    var pri = pr + inv
-    document.getElementById("priceAddinv").innerHTML = pri;
+            var pr = 0
+
+            for (i = 0; i < count; i++) {
+                tableContent = tableContent + "<tr class=" + tr + ">"
+                    + "<td class=" + td + ">" + parsed.bill[i].ID_TRN_taxlnvoice +
+                    "</td> <td class=" + td + ">" + parsed.bill[i].license_plate +
+                    "</td> <td class=" + td + ">" + parsed.bill[i].type +
+                    "</td> <td class=" + td + ">" + parsed.bill[i].color +
+                    "</td> <td class=" + td + ">  " + parsed.bill[i].price +
+                    "</tr>";
+                pr = pr + parseInt(parsed.bill[i].price);
+            }
+
+            var tableFooter = "</table>";
+
+            document.getElementById("BVtable").innerHTML = tableHeader + tableContent + tableFooter;
+            document.getElementById("priceAll").innerHTML = pr;
+            var inv = pr * (5 / 100)
+            document.getElementById("inv").innerHTML = inv;
+
+            var pri = pr + inv
+            document.getElementById("priceAddinv").innerHTML = pri;
+        },
+        error: function (e) {
+            console.log("ERROR: ", e);
+        }
+    });
+
 }
 
-function insertItemBill(){
+function insertItemBill() {
 
 }
--- router/buy.js
+++ router/buy.js
@@ -6,30 +6,72 @@ var MongoClient = mongodb.MongoClient;
 var url = "mongodb://gigabug:gigabug1234@ds141351.mlab.com:41351/gigabug";
 app.use(bodyParser.json());
 
-app.get("/api/buy/invoice/getItem", (req, res) => {
+app.post("/api/buy/invoice/getItem", (req, res) => {
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("gigabug");
-      
-        var id ={
-            ID_TRN_buy_contract:req.body.id
+
+        var id = {
+            ID_TRN_buy_bill: req.body.id
         }
-      
-        console.log(user)
-        dbo.collection('TRN_buy_contract').find(id).count((err, result) => {
+        var respons = [];
+
+        console.log(req.body)
+        dbo.collection('TRN_buy_bill').find(id).toArray((err, result) => {
             if (err) {
                 res.sendStatus(404)
             } else {
-                console.log(result)
-                if (result > 0) {
-                
-                    res.send('true')
 
-
-                } else {
-                  
-                    res.send('false')
+                var idc = {
+                    ID_TRN_buy_contract: result[0].ID_TRN_buy_contract
                 }
+                respons.push(result)
+
+                dbo.collection('TRN_buy_contract').find(idc).toArray((err, result) => {
+                    if (err) {
+                        res.sendStatus(404)
+                    } else {
+                        respons.push(result)
+                        var idcustomer = {
+                            ID_MST_costomer: result[0].ID_MST_customer
+                        }
+                        var idstock = {
+                            ID_MST_stock: result[0].ID_MST_stock
+                        }
+                        var idemployee = {
+                            ID_MST_employee: result[0].ID_MST_employee
+                        }
+                        dbo.collection('MST_customer').find(idcustomer).toArray((err, result) => {
+                            if (err) {
+                                res.sendStatus(404)
+                            } else {
+                                respons.push(result)
+
+                                dbo.collection('MST_employee').find(idemployee).toArray((err, result) => {
+                                    if (err) {
+                                        res.sendStatus(404)
+                                    } else {
+                                        respons.push(result)
+                                        dbo.collection('MST_stock').find(idstock).toArray((err, result) => {
+                                            if (err) {
+                                                res.sendStatus(404)
+                                            } else {
+                                                respons.push(result)
+                                              res.send(respons)
+
+                                            }
+
+                                        })
+                                    }
+
+                                })
+                            }
+
+                        })
+
+                    }
+
+                })
 
             }
 
