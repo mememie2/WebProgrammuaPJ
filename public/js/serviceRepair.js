85b0aa1dc2d90dc8ff6f00da75d0dea3cae19989
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-03-25 19:24:14 +0700

    rpDetail add and delete row finish!!

--- public/js/serviceRepair.js
+++ public/js/serviceRepair.js
@@ -1,4 +1,5 @@
 var check = 0;
+//addRow
 function addToTable() {
     var v1 = document.getElementById("nameSpare").value;
     var v2 = document.getElementById("priceSpare").value;
@@ -13,9 +14,9 @@ function addToTable() {
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        cell1.innerHTML = '<td class="tdBest">' + v1 + '</td>';
-        cell2.innerHTML = '<td class="tdBest">' + v2 + '</td>';
-        cell3.innerHTML = '<td class="tdBest"><center><a onclick="deleteToTable(this)" class="button delete">ลบ</a></center></td>';
+        cell1.innerHTML = '<td >' + v1 + '</td>';
+        cell2.innerHTML = '<td >' + v2 + '</td>';
+        cell3.innerHTML = '<td ><center><a onclick="deleteToTable()" class="button delete">ลบ</a></center></td>';
         check = check + 1;
     } else {
         window.alert("กรุณากรอกข้อมูลให้ถูกต้องและครบถ้วน")
@@ -23,7 +24,22 @@ function addToTable() {
 
 }
 
-function deleteToTable(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("listTable").deleteRow(i);
+//deleteRow
+function deleteToTable() {
+    var index, table = document.getElementById('listTable');
+    for (var i = 1; i < table.rows.length; i++) {
+        table.rows[i].cells[2].onclick = function () {
+
+            index = this.parentElement.rowIndex;
+            table.deleteRow(index);
+            console.log(index);
+        };
+
+    }
+}
+
+
+//connect DB
+function connectDB() {
+
 }
